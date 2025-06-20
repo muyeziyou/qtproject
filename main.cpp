ba9c7254c109a1340ec3c1cda74a0e9933193e57
myzy <ziyoumuye41@gmail.com>
2025-06-20 20:12:55 +0800

    改了下mian文件

--- main.cpp
+++ main.cpp
@@ -11,7 +11,7 @@ int main(int argc, char* argv[]) {
     QApplication a(argc, argv);
     QtWidgetsApplication1 w;
     w.setGeometry(500, 100, 800, 600);
-    lableacon(&w);
+   // lableacon(&w);
    // w.show();
     return a.exec();
 }
